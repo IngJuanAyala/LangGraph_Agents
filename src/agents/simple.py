70f34ce4795afcdaa6e386c59163284eda6dbcd6
Juan Ayala <juanayalasalazar1@gmail.com>
2026-07-23 00:00:14 -0500

    test contact messages into nodes

--- src/agents/simple.py
+++ src/agents/simple.py
@@ -1,17 +1,24 @@
 
 from langgraph.graph import StateGraph, START, END
-from IPython.display import Image, display
+from IPython.display import display
+from langgraph.graph import MessagesState
+from langchain_core.messages import AIMessage
 
-class State:
+class State(MessagesState):
     customer_name: str
     my_age: int
 
 def node_1(state: State):
+    history = state["messages"]
     if state.get("customer_name") is None:
-        state["customer_name"] = "Juan1"
-    if state.get("my_age") is None:
-        state["my_age"] = 36
-    return state
+        return {
+            "customer_name": "Juan Ayl"
+        }
+    else:
+        ai_msg = AIMessage(content="Hello, how can I help you today?")
+        return {
+            "messages": [ai_msg]
+        }
 
 builder = StateGraph(State)
 builder.add_node("node_1", node_1)
